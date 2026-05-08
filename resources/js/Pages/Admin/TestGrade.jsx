fd7dc2ce7bf22170546884aa1b209ce80ca1dc20
paullara <zaykinn66@gmail.com>
2026-05-08 17:17:12 +0800

    started refactoring form137 feature

--- resources/js/Pages/Admin/TestGrade.jsx
+++ resources/js/Pages/Admin/TestGrade.jsx
@@ -3,7 +3,7 @@ import Admin from "@/Layouts/AdminLayout";
 export default function TestGrade() {
     return (
         <Admin>
-            <h1>Hello</h1>
+            <h1>Hello Nigga</h1>
         </Admin>
     );
 }
