1db954e664404491a25ee799229aa96c8897cf97
paullara <zaykinn66@gmail.com>
2026-05-11 08:41:44 +0800

    form component

--- resources/js/Components/Grades/SearchBar.jsx
+++ resources/js/Components/Grades/SearchBar.jsx
@@ -1,1 +1,42 @@
-export default function SearchBar() {}
+export default function SearchBar({
+    lrn,
+    setLrn,
+    handleSearch,
+    searching,
+    handlePrint,
+    onReset,
+}) {
+    return (
+        <form
+            onSubmit={handleSearch}
+            className="mb-2 flex gap-2 justify-end items-end no-print w-full"
+        >
+            <input
+                value={lrn}
+                onChange={(e) => setLrn(e.target.value)}
+                placeholder="Enter LRN"
+                className="border rounded px-3 py-2"
+            />
+
+            <button className="bg-indigo-600 text-white px-4 py-2 rounded">
+                {searching ? "Searching..." : "Search"}
+            </button>
+
+            <button
+                type="button"
+                className="bg-gray-500 text-white px-4 py-2 rounded"
+                onClick={onReset}
+            >
+                Reset
+            </button>
+
+            <button
+                type="button"
+                onClick={handlePrint}
+                className="bg-green-600 text-white px-4 py-2 rounded"
+            >
+                Print Grades
+            </button>
+        </form>
+    );
+}
