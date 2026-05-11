4dd1e1c1ea41606d4ecbfc17eb7d6e5d62378806
paullara <zaykinn66@gmail.com>
2026-05-11 08:46:46 +0800

    section title

---
+++ resources/js/Components/Grades/SectionTitle.jsx
@@ -0,0 +1,7 @@
+export default function SectionTitle({ title }) {
+    return (
+        <div className="w-full text-[13px] bg-gray-400 flex items-center justify-center mt-2">
+            <h1 className="font-semibold">{title}</h1>
+        </div>
+    );
+}
