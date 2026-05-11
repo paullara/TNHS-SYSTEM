2d04ae4a6980808a9a05f0da664a4e0311347063
paullara <zaykinn66@gmail.com>
2026-05-11 08:27:41 +0800

    finished main grade layout

--- resources/js/Pages/Admin/TestGrade.jsx
+++ resources/js/Pages/Admin/TestGrade.jsx
@@ -63,7 +63,103 @@ export default function TestGrade() {
 
     return (
         <Admin>
-            <SearchBar />
+            <SearchBar
+                lrn={lrn}
+                setLrn={setLrn}
+                handleSearch={handleSearch}
+                handlePrint={handlePrint}
+                searching={searching}
+                onReset={() => {
+                    setLrn("");
+                    setHasSearched(false);
+                    setData([]);
+                    setError(null);
+                }}
+            />
+
+            {loading && <p>Loading...</p>}
+            {error && <p className="text-red-500">{error}</p>}
+            <div className="printable p-3 w-full flex justify-center items-center">
+                {!hasSearched && (
+                    <p>Please search a student LRN to display grade data.</p>
+                )}
+
+                {hasSearched && data.length === 0 && !loading && !error && (
+                    <p>No grades found for this LRN.</p>
+                )}
+
+                {data.length > 0 && (
+                    <div className="w-full bf-red-500">
+                        <ReportHeader />
+                        <LearnerInformation data={data[0]} />
+                        <EligibilitySection />
+
+                        {data
+                            .sort((a, b) => a.sy.localeCompare(b.sy))
+                            .map((item, index) => (
+                                <div key={index} className="mb-10">
+                                    {item.first_sem.subjects.length > 0 && (
+                                        <>
+                                            <SemesterHeader
+                                                item={item}
+                                                semesterLabel="1ST"
+                                            />
+
+                                            <GradeTable
+                                                subjects={
+                                                    item.first_sem.subjects
+                                                }
+                                                label="1st Semester"
+                                                columns={["q1", "q2"]}
+                                                avg={item.first_sem.average}
+                                                finalKey="first_sem_final"
+                                                adviser={item?.section?.adviser}
+                                            />
+
+                                            <RemedialHeader />
+
+                                            <RemedialTable
+                                                subjects={
+                                                    item.first_sem.subjects
+                                                }
+                                                finalKey="first_sem_final"
+                                            />
+                                        </>
+                                    )}
+
+                                    {item.second_sem.subjects.length > 0 && (
+                                        <>
+                                            <SemesterHeader
+                                                item={item}
+                                                semesterLabel="2ND"
+                                            />
+
+                                            <GradeTable
+                                                subjects={
+                                                    item.second_sem.subjects
+                                                }
+                                                label="2nd Semester"
+                                                columns={["q3", "q4"]}
+                                                avg={item.second_sem.average}
+                                                finalKey="second_sem_final"
+                                                adviser={item?.section?.adviser}
+                                            />
+
+                                            <RemedialHeader />
+
+                                            <RemedialTable
+                                                subjects={
+                                                    item.second_sem.subjects
+                                                }
+                                                finalKey="second_sem_final"
+                                            />
+                                        </>
+                                    )}
+                                </div>
+                            ))}
+                    </div>
+                )}
+            </div>
         </Admin>
     );
 }
